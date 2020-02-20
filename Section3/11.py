b9b1d0586ebbf5b58fcb6461372b01bb7aa5206f
Juan Antonio Marin Sanchez <avalanch.psp@gmail.com>
2020-02-20 19:25:01 +0100

    Section 3: Python Numbers and Coding Exercise 1

---
+++ Section3/11.py
@@ -0,0 +1,15 @@
+# Python Numbers
+print(2 + 1)
+print(2 - 1)
+print(2 * 2)
+print(3 / 2)
+print(7 / 4)
+print(7 % 4)  # Modulo
+print(23 % 2)
+print(20 % 2)
+print(2 ** 3)
+print(2 + 10 * 10 + 3) # Order (10 * 10) + 2 + 3
+
+# Coding Exercise 1: Numbers: Simple Arithmetic
+# 2*50
+print(2*50)
