1461f3850d855a758a6ed87af654ec8661b5e6a5
Juan Antonio Marin Sanchez <avalanch.psp@gmail.com>
2020-02-24 17:51:17 +0100

    Section 3: Introduction to String and Coding Exercise 2

---
+++ Section3/14.py
@@ -0,0 +1,11 @@
+print('hello')
+print("world")
+print('this is also a string')
+print(" I'm going on a run ")
+print('hello \nworld')
+print('hello \tworld')
+print(len('hello'))
+print(len('I am'))
+
+# Coding Exercise 2: Quic Print Check
+print("Hello World")
