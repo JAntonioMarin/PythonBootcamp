682215ff714d9b55e7f9201d42fe65598c464d32
Juan Antonio Marin Sanchez <avalanch.psp@gmail.com>
2020-02-24 18:06:50 +0100

    Section 3: Indexing and slicing with Strings, and Exercise 3,4

---
+++ Section3/15.py
@@ -0,0 +1,18 @@
+mystring = "Hello World"
+print(mystring)
+print(mystring[0])
+print(mystring[-1])
+
+print(mystring)
+print(mystring[2:])
+print(mystring[:3])
+print(mystring[3:7])
+print(mystring[::2])
+
+print(mystring[2:7:2])
+
+# Coding Exercise 3
+print('Hello World'[8])
+
+# Coding Exercise 4
+print('tinker'[1:4])
