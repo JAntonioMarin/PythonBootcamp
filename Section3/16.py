5db3fa27858e901de3ed9a7f2ce6ef416bdeb6a3
Juan Antonio Marin Sanchez <avalanch.psp@gmail.com>
2020-02-25 15:09:26 +0100

    Section 3: String Properties and Methods

---
+++ Section3/16.py
@@ -0,0 +1,26 @@
+# Immutability
+
+name = "Sam"
+print(name)
+# name[0] = 'P' # Error
+
+last_letters = name[1:]
+print(last_letters)
+
+x = 'Hello World'
+x = x + " it is a beatiful outside!"
+print(x)
+
+
+letter = 'z'
+print(letter * 10)
+
+print(2+3)
+print('2'+'3')
+
+dot = 'Hello World'
+print(dot.upper())
+print(dot.lower())
+print(dot.split())
+print(len(dot.split()))
+print(dot.split('l'))
