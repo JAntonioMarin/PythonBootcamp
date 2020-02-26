5d9da034c09f1f39860e7b7a69c175edbea721a6
Juan Antonio Marin Sanchez <avalanch.psp@gmail.com>
2020-02-26 15:37:38 +0100

    Section3: Sets in Python

---
+++ Section3/25.py
@@ -0,0 +1,11 @@
+myset = set()
+print(myset)
+myset.add(1)
+print(myset)
+myset.add(2)
+print(myset)
+myset.add(2) #Unique values
+print(myset)
+
+mylist = [2,1,1,1,1,1,2,2,2,1,1,1,3,3,3,3]
+print(set(mylist))
