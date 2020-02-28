0f5540516e7b21d1dd395a2a2c4325038d3f530e
Juan Antonio Marin Sanchez <avalanch.psp@gmail.com>
2020-02-28 13:00:38 +0100

    Section 5: If, elif and else Statements in Python

---
+++ Section5/33.py
@@ -0,0 +1,24 @@
+if True:
+    print('Its true!')
+
+hungry = True
+if hungry:
+    print('Feed me!')
+
+hungry = False
+if hungry:
+    print('Feed me!')
+else:
+    print('Im not hungry')
+
+
+loc = 'Bank'
+
+if loc == 'Auto Shop':
+    print('Cars are cool!')
+elif loc == 'Store':
+    print('Welcome to the store!')
+elif loc == 'Bank':
+    print('Money is cool')
+else:
+    print('I dont know much.')
